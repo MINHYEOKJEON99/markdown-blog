a421df579464de5504520db93a4196ed4e48c771
MINHYEOKJEON <mari394337@gmail.com>
2025-05-11 11:26:02 +0900

    ui: header nav

---
+++ components/main-nav.tsx
@@ -0,0 +1,8 @@
+export default function MainNav() {
+  return (
+    <nav className="h-12 bg-gray-200 px-4 flex items-center justify-between">
+      <div>MINHYEOKJEON's Blog</div>
+      <section>Menus</section>
+    </nav>
+  );
+}
