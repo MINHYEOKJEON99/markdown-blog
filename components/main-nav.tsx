9a1bfda5c7cefc58e0d2850f52cd3ca685080a3b
MINHYEOKJEON <mari394337@gmail.com>
2025-05-11 11:33:08 +0900

    fix: nav ui수정

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,10 @@
 export default function MainNav() {
   return (
-    <header className="h-12 bg-gray-200 px-4 flex items-center justify-between">
-      <div>MINHYEOKJEON's Blog</div>
-      <nav>Menus</nav>
-    </header>
+    <div className="w-full flex justify-center">
+      <header className="h-12 w-full max-w-[1040px] rounded-full border border-gray-300 mx-8 mt-8  shadow-xl px-4 flex items-center justify-between">
+        <div>MINHYEOKJEON's Blog</div>
+        <nav>Menus</nav>
+      </header>
+    </div>
   );
 }
